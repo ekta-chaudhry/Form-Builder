bfe411b8ad0b5bcff865b74d4c1a4733190a90b3
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-10-22 03:02:04 +0530

    Added styles

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let fieldCount = 0;
 function addFieldToPreview() {
-    if(fieldCount >= 5) {
+    if(fieldCount >= 7) {
         alert("Maximum 5 fields can be added!");
         return;
     }
 
-    if(!document.getElementById('fieldLabelInput').value) {
+    if(!document.getElementById('field-label-input').value) {
         alert("Please add a label to the field!");
         return;
     }
@@ -15,19 +15,42 @@ function addFieldToPreview() {
     const selectElem = document.querySelector('select');
 
     const newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'preview-field-div');
 
+    if(selectElem.value == "button") {
+        const button = document.createElement('button');
+        button.innerText = document.getElementById('field-label-input').value;
+        button.setAttribute('class', 'add-field-button');
+        newDiv.appendChild(button);
+        previewDiv.appendChild(newDiv);
+        return;
+    }
+    
+    const inputDiv = document.createElement('div');
     const inputElem = document.createElement('input');
     inputElem.setAttribute('type', selectElem.value);
     inputElem.setAttribute('id', `field${fieldCount}`);
-
+    if(selectElem.value == "text") {
+        inputElem.setAttribute('class', 'text-field');
+    }
+    const labelDiv = document.createElement('div');
     const labelElem = document.createElement('label');
     labelElem.setAttribute('for', `field${fieldCount}`);
-    labelElem.innerText = document.getElementById('fieldLabelInput').value;
+    labelElem.setAttribute('class', 'field-label');
+    labelElem.innerText = document.getElementById('field-label-input').value;
     
-    newDiv.appendChild(labelElem);
-    newDiv.appendChild(inputElem);
+    if(selectElem.value == "text") {
+        inputDiv.appendChild(inputElem);
+        labelDiv.appendChild(labelElem);
+        newDiv.appendChild(labelDiv);
+        newDiv.appendChild(inputDiv);
+    }else{
+        newDiv.appendChild(labelElem);
+        newDiv.appendChild(inputElem);
+    }
+    
     previewDiv.appendChild(newDiv);
 
-    document.getElementById('fieldLabelInput').value = "";
+    document.getElementById('field-label-input').value = "";
     
 }
